227f06ffd01b2be557fb5e2f892acb735ba6f922
EdwinRodger <vaidiktest@gmail.com>
2022-11-01 12:21:20 +0530

    date wont show when set to 01-01-0001

--- src/main/routes.py
+++ src/main/routes.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 from flask import Blueprint, redirect, render_template, url_for
 from src.models import Manga
+
+d = datetime.strptime("0001-01-01", "%Y-%m-%d")
+date = d.date()
 
 main = Blueprint("main", __name__)
 
@@ -8,7 +13,7 @@ main = Blueprint("main", __name__)
 @main.route("/home")
 def home():
     mangas = Manga.query.all()
-    return render_template("home.html", title="Home", mangas=mangas)
+    return render_template("home.html", title="Home", mangas=mangas, date=date)
 
 
 @main.route("/about")
