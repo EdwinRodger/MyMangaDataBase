5c5bf0894db67e095c4365f67959292c5c37db09
EdwinRodger <vaidiktest@gmail.com>
2022-11-01 11:56:56 +0530

    add logger and host to waitress

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from src.models import Manga
 from src import create_app, db
 from src.utils import check_dotenv
 import waitress
+import logging
 
 app = create_app()
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     check_dotenv()
     with app.app_context():
         db.create_all()
-    waitress.serve(app=app, port=6070)
+    waitress.serve(app=app, host='127.0.0.1', port=6070)
