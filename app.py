fdc80e43ed2886ee91af8d1df6bbb3a09e6c39c4
EdwinRodger <vaidiktest@gmail.com>
2022-11-01 11:37:22 +0530

    using waitress for deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src.models import Manga
 from src import create_app, db
 from src.utils import check_dotenv
+import waitress
 
 app = create_app()
 
@@ -8,4 +9,4 @@ if __name__ == "__main__":
     check_dotenv()
     with app.app_context():
         db.create_all()
-    app.run(port=6070)
+    waitress.serve(app=app, port=6070)
--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///manga.db"
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = str(os.environ.get("SECRET_KEY"))
