68f2963bd58b1f524118166dcf6218c97c0fed67
EdwinRodger <codevaidik@gmail.com>
2022-12-05 20:37:24 +0530

    removed untrusted code

--- app_runner.py
+++ app_runner.py
@@ -1,5 +1,4 @@
 import os
-import platform
 
 try:
     if os.path.exists("Pipfile"):
@@ -7,19 +6,12 @@ try:
             print("pipenv run app.py")
             os.system("pipenv run app.py")
         else:
-            if platform.system() == "Windows":
-                print("pip install pipenv")
-                os.system("pip install pipenv")
-            else:
-                print("pip3 install pipenv")
-                os.system("pip3 install pipenv")
-            print("pipenv install")
-            os.system("pipenv install")
-            print("pipenv run app.py")
-            os.system("pipenv run app.py")
+            print("pip install pipenv")
+            os.system("pip install pipenv")
+        print("pipenv install")
+        os.system("pipenv install")
+        print("pipenv run app.py")
+        os.system("pipenv run app.py")
 except Exception as e:
     print(e)
-    if platform.system() == "Windows":
-        os.system("pause")
-    else:
-        os.system("read -p 'Press enter key to resume ...'")
+    os.system("pause")
