e1e56fe0413014dce1392f0b27bbd2c907e20f64
EdwinRodger <codevaidik@gmail.com>
2022-12-17 17:22:00 +0530

    .env file won't get exported

--- src/main/utils.py
+++ src/main/utils.py
@@ -13,7 +13,6 @@ today_date = datetime.date(datetime.today())
 
 def export_backup():
     with ZipFile(f"src\\MMDB-Export-{today_date}.zip", "w") as zf:
-        zf.write(".env")
         zf.write("instance\\manga.db")
 
 
