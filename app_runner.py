94c2df06e8ba08e58a3eea246e91a0fd0fc0c80a
EdwinRodger <codevaidik@gmail.com>
2023-01-17 20:26:58 +0530

    added versions when starting the program

--- app_runner.py
+++ app_runner.py
@@ -1,9 +1,11 @@
 import errno
 import os
 
+VERSION = "1.5.0"
+
 if os.path.exists("Pipfile"):
     if os.path.exists("Pipfile.lock"):
-        print("Starting MyMangaDataBase! Please wait...")
+        print(f"Starting MyMangaDataBase {VERSION}! Please wait...")
         os.system("pipenv run python app.py")
     else:
         print("\nInstalling 'pipenv' to make virtual environment\n")
@@ -12,7 +14,7 @@ if os.path.exists("Pipfile"):
             "\nCreating virtual environment and installing required packages from pipfile via pipenv\n"
         )
         os.system("pipenv install")
-        print("\nStarting MyMangaDataBase! Please wait...\n")
+        print(f"\nStarting MyMangaDataBase {VERSION}! Please wait...\n")
         os.system("pipenv run python app.py")
 else:
     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), "Pipfile")
