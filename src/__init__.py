02292c353d8b45df764aede2e1df6d262eef1c17
EdwinRodger <codevaidik@gmail.com>
2023-01-26 21:53:52 +0530

    made random search placeholder text

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -25,7 +27,18 @@ def create_app(config_class=Config):
     # Pass Stuff To Navbar
     @app.context_processor
     def base():
+        from src.models import Manga
+
+        # Below logic finds all the manga from database, take random manga, get its title and then send it to search bar as a place holder
+        manga = Manga.query.order_by(Manga.title.name).all()
+        max = len(manga)
+        index = random.randint(0, (max - 1))
+        if index != 0:
+            manga = manga[index]
+            manga_title = manga.title
+        else:
+            manga_title = "Search"
         form = SearchBar()
-        return dict(navsearch=form)
+        return dict(navsearch=form, manga_title=manga_title)
 
     return app
