c053e5efd7ef6168f5b0bd699be635034818b653
EdwinRodger <codevaidik@gmail.com>
2023-04-23 15:03:10 +0530

    updated routes to error pages

--- src/errors/handlers.py
+++ src/errors/handlers.py
@@ -3,13 +3,18 @@ from flask import Blueprint, render_template
 errors = Blueprint("errors", __name__)
 
 
-@errors.route("/errors/<int:error>")
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template("errors/404.html"), 404
 
 
-@errors.route("/errors/<int:error>")
 @errors.app_errorhandler(500)
 def error_500(error):
     return render_template("errors/500.html"), 500
+
+@errors.route("/errors/<int:error>")
+def err(error):
+    if error == 404:
+        return render_template("errors/404.html")
+    if error == 500:
+        return render_template("errors/500.html")
