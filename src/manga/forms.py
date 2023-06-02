03dfd166617f9d56b575a30d7418a3874cb21313
EdwinRodger <codevaidik@gmail.com>
2023-06-02 14:49:51 +0530

    made manga list table

---
+++ src/manga/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
+from wtforms import (
+    DateField,
+    IntegerField,
+    SelectField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+)
+from wtforms.validators import DataRequired, Length
+
+class MangaForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(min=2)])
+    cover = FileField("Cover Image", validators=[FileAllowed(["jpg", "png"])])
+    start_date = DateField("Start Date")
+    end_date = DateField("End Date")
+    volume = IntegerField("Volume")
+    chapter = IntegerField("Chapter")
+    score = SelectField("Score", choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    status = SelectField(
+        "Status",
+        choices=[
+            "Reading",
+            "Completed",
+            "On hold",
+            "Dropped",
+            "Plan to read",
+            "Rereading",
+        ],
+    )
+    description = TextAreaField("Description")
+    tags = StringField("Tags")
+    author = StringField("Author")
+    artist = StringField("Artist")
+    submit = SubmitField("Add")
+    update = SubmitField("Update")
+    
--- src/manga/routes.py
+++ src/manga/routes.py
@@ -1,7 +1,40 @@
-from flask import (Blueprint, render_template,)
+from flask import (Blueprint, render_template, flash, redirect, url_for)
+from src.manga.forms import MangaForm
+from src.models import Manga
+from src import db
 
 manga = Blueprint("manga", __name__, url_prefix="/manga")
 
 @manga.route("/list")
 def manga_list():
-    return render_template("manga/manga-list.html", title = "Manga List", current_section = "Manga")
+    manga_list = Manga.query.order_by(Manga.title.name).all()
+    return render_template(
+        "manga/manga-list.html", title = "Manga List", current_section = "Manga", manga_list=manga_list
+    )
+
+# Add New Manga
+@manga.route("/new", methods=["GET", "POST"])
+def add_manga():
+    form = MangaForm()
+    if form.validate_on_submit():
+        manga = Manga(
+            title=form.title.data,
+            cover=form.cover.data,
+            start_date=form.start_date.data,
+            end_date=form.end_date.data,
+            volume=form.volume.data,
+            chapter=form.chapter.data,
+            status=form.status.data,
+            score=form.score.data,
+            description=form.description.data,
+            tags=form.tags.data,
+            author=form.author.data,
+            artist=form.artist.data,
+        )
+        db.session.add(manga)
+        db.session.commit()
+        flash(f"{form.title.data} is added!", "success")
+        return redirect(url_for("manga.manga_list"))
+    return render_template(
+        "manga/create-manga.html", title="New Manga", form=form, legend="New Manga"
+    )
