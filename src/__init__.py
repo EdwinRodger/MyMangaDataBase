a4b5bf470426e455a0bb8cf121046cbc135739cd
EdwinRodger <codevaidik@gmail.com>
2023-06-02 13:25:26 +0530

    layout manga list html

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,9 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     from src.home.routes import home
+    from src.manga.routes import manga
+
     app.register_blueprint(home)
+    app.register_blueprint(manga)
 
     return app
---
+++ src/manga/routes.py
@@ -0,0 +1,7 @@
+from flask import (Blueprint, render_template,)
+
+manga = Blueprint("manga", __name__, url_prefix="/manga")
+
+@manga.route("/list")
+def manga_list():
+    return render_template("manga-list.html", title = "Manga List", current_section = "Manga")
