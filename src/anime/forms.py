d143cd88c49a4725da6957e8ce15857cc7cffccc
EdwinRodger <codevaidik@gmail.com>
2023-06-02 17:18:00 +0530

    added anime list and other functionalities

---
+++ src/anime/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
+from wtforms import (
+    DateField,
+    IntegerField,
+    SelectField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+)
+from wtforms.validators import DataRequired, Length
+
+class AnimeForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(min=2)])
+    cover = FileField("Cover Image", validators=[FileAllowed(["jpg", "png"])])
+    start_date = DateField("Start Date")
+    end_date = DateField("End Date")
+    episode = IntegerField("Episode")
+    score = SelectField("Score", choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    status = SelectField(
+        "Status",
+        choices=[
+            "Watching",
+            "Completed",
+            "On hold",
+            "Dropped",
+            "Plan to watch"
+        ],
+    )
+    description = TextAreaField("Description")
+    tags = StringField("Tags")
+    submit = SubmitField("Add")
+    update = SubmitField("Update")
+    
--- src/anime/routes.py
+++ src/anime/routes.py
@@ -1,7 +1,46 @@
-from flask import (Blueprint, render_template,)
+from flask import (Blueprint, render_template, flash, redirect, url_for)
+from src.anime.forms import AnimeForm
+from src.models import Anime
+from src import db
 
 anime = Blueprint("anime", __name__, url_prefix="/anime")
 
 @anime.route("/list")
 def anime_list():
-    return render_template("anime/anime-list.html", title = "Anime List", current_section = "Anime")
+    anime_list = Anime.query.order_by(Anime.title.name).all()
+    return render_template("anime/anime-list.html", title = "Anime List", current_section = "Anime", anime_list=anime_list, sort_function = "All")
+
+# Add New Anime
+@anime.route("/new", methods=["GET", "POST"])
+def new_anime():
+    form = AnimeForm()
+    if form.validate_on_submit():
+        anime = Anime(
+            title=form.title.data,
+            cover=form.cover.data,
+            start_date=form.start_date.data,
+            end_date=form.end_date.data,
+            episode=form.episode.data,
+            status=form.status.data,
+            score=form.score.data,
+            description=form.description.data,
+            tags=form.tags.data,
+        )
+        db.session.add(anime)
+        db.session.commit()
+        flash(f"{form.title.data} is added!", "success")
+        return redirect(url_for("anime.anime_list"))
+    return render_template(
+        "anime/create-anime.html", title="New Anime", form=form, legend="New Anime"
+    )
+
+# Sort Anime
+@anime.route("/list/<string:sort_function>", methods=["GET", "POST"])
+def sort_anime(sort_function):
+    anime_list = Anime.query.filter_by(status=sort_function).order_by(Anime.title.name).all()
+    return render_template(
+        "anime/anime-list.html",
+        title=f"{sort_function} Anime",
+        anime_list=anime_list,
+        sort_function = sort_function
+    )
