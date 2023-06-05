87f98c94e5f1c07e2cb62039a393878bcadd05f1
EdwinRodger <codevaidik@gmail.com>
2023-06-05 17:50:58 +0530

    optimized check for update code

--- src/home/utils.py
+++ src/home/utils.py
@@ -1,7 +1,7 @@
 from src.models import Manga, Anime
 import statistics
 from collections import Counter
-from urllib.request import Request, urlopen
+import requests
 import os
 import hashlib
 import json
@@ -80,28 +80,37 @@ def anime_overview_data():
     return anime_overview_data
 
 def check_for_update():
-    """Checks for software update using github's api.
-    It reads api page contents and converts it into hash which is saved in `versionhash.txt` file.
-    It repeats the process again when the program is opened and checks if the older session
-    versionhash is equal to this session's versionhash, if it is equal then the program is
-    up-to-date but if the hash changes that means a new version is published on github and this
-    function prompts user to update to latest function by sending them to latest github release page
-    """
-    url = Request("https://api.github.com/repos/EdwinRodger/MyMangaDataBase/tags")
-    response = urlopen(url).read()
-    if not os.path.exists("json/versionhash.json"):
-        current_hash = hashlib.sha224(response).hexdigest()
-        with open("json/versionhash.json", "w", encoding="UTF-8") as file:
-            json.dump({"current_hash":current_hash}, file, indent=4)
+    # API URL to fetch the latest tags/releases
+    url = "https://api.github.com/repos/EdwinRodger/MyMangaDataBase/tags"
+    
+    # Fetch the JSON response from the API
+    response = requests.get(url).json()
+
+    # File path to store the version hash
+    version_hash_file = "json/versionhash.json"
+    
+    # Calculate the hash of the response
+    current_hash = hashlib.sha224(str(response).encode("utf-8")).hexdigest()
+
+    # Check if the version hash file exists
+    if not os.path.exists(version_hash_file):
+        # If the file doesn't exist, create it and store the current hash
+        with open(version_hash_file, "w", encoding="utf-8") as file:
+            json.dump({"current_hash": current_hash}, file, indent=4)
     else:
-        with open("json/versionhash.json", "r", encoding="UTF-8") as file:
-            current_hash = json.load(file)
-        new_hash = hashlib.sha224(response).hexdigest()
-        if new_hash == current_hash["current_hash"]:
-            return False
-        else:
-            current_hash = hashlib.sha224(response).hexdigest()
-            with open("json/versionhash.json", "w", encoding="UTF-8") as file:
-                json.dump({"current_hash":current_hash}, file, indent=4)
-            return True
+        # If the file exists, read the stored hash
+        with open(version_hash_file, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            
+            # Check if the current hash matches the stored hash
+            if current_hash == data["current_hash"]:
+                # No update available
+                return False
+
+        # Update the hash in the file to reflect the latest version
+        with open(version_hash_file, "w", encoding="utf-8") as file:
+            json.dump({"current_hash": current_hash}, file, indent=4)
+
+    # An update is available
+    return True
 
