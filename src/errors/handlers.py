9e0160c3a5624f05d0bbca24bbfb874678861272
EdwinRodger <codevaidik@gmail.com>
2023-12-08 17:21:37 +0530

    New: Method not allowed (405) error page

--- src/errors/handlers.py
+++ src/errors/handlers.py
@@ -8,6 +8,8 @@ errors = Blueprint("errors", __name__)
 
 # List of reactions for 404 error
 reactions_404 = ["facepalm", "mad", "smack"]
+# List of reactions for 405 error
+reactions_405 = ["no", "bleh"]
 # List of reactions for 500 error
 reactions_500 = ["nervous", "sorry", "surprised", "sweat", "confused"]
 # Base URL for the API
@@ -17,7 +19,10 @@ api_base_url = "https://api.otakugifs.xyz/gif"
 # Function to retrieve the GIF URL for a given reaction
 def get_gif_url(reaction):
     url = f"{api_base_url}?reaction={reaction}&format=WebP"
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except:
+        return None
     if response.status_code == 200:
         data = response.json()
         gif_url = data["url"]
@@ -29,6 +34,8 @@ def get_gif_url(reaction):
 def gif_url(code):
     if code == 404:
         reactions = reactions_404
+    elif code == 405:
+        reactions = reactions_405
     else:
         reactions = reactions_500
     # Randomly select a reaction from the given list
@@ -46,6 +53,12 @@ def error_404(error):
     return render_template("errors/404.html", url=url), 404
 
 
+@errors.app_errorhandler(405)
+def error_405(error):
+    url = gif_url(405)
+    return render_template("errors/405.html", url=url), 405
+
+
 @errors.app_errorhandler(500)
 def error_500(error):
     url = gif_url(500)
@@ -57,6 +70,9 @@ def err(error):
     if error == 404:
         url = gif_url(404)
         return render_template("errors/404.html", url=url)
+    if error == 405:
+        url = gif_url(405)
+        return render_template("errors/405.html", url=url)
     if error == 500:
         url = gif_url(500)
         return render_template("errors/500.html", url=url)
