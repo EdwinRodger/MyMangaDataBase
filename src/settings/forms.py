4eb6e19fec2a5fc7fcedd0242afe7b608827bfb7
EdwinRodger <codevaidik@gmail.com>
2023-12-15 20:15:08 +0530

    New: "Image Overlay Card" layout setting

--- src/settings/forms.py
+++ src/settings/forms.py
@@ -6,6 +6,6 @@ class SettingsForm(FlaskForm):
     theme = SelectField("Theme", choices=["Dark", "Light"])
     enable_logging = SelectField("Enable Logging", choices=["Yes", "No"])
     truncate_title = SelectField("Truncate Title", choices=["Yes", "No"])
-    layout = SelectField("Layout", choices=["Table", "Card"])
+    layout = SelectField("Layout", choices=["Table", "Card", "Image Overlay Card"])
     mmdb_promotion = SelectField("MMDB Promotion", choices=["Yes", "No"])
     save = SubmitField("Save")
